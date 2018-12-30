b89700c4b220ac699c69b48f97853adaf76a32a0
dhoonjang <dhoonjang@gmail.com>
2018-12-30 16:49:45 +0900

    before block

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-const name = "Nicolas",
-  age = 24,
-  gender = "male"
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`
+const donghoon = new Human("dh", 20, "male");
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!!`
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(donghoon));
 
 export {};
